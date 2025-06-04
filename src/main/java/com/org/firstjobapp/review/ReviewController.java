58980aceb2c34319a7c3161a85feaef60742d84b
kumkum <kumkumbablani85798@gmail.com>
2025-06-04 14:38:22 +0530

    Review Implentation Completed

--- src/main/java/com/org/firstjobapp/review/ReviewController.java
+++ src/main/java/com/org/firstjobapp/review/ReviewController.java
@@ -22,7 +22,40 @@ public class ReviewController {
     }
 
     @PostMapping("/reviews")
-    public ResponseEntity<String> addReview(@PathVariable Long companyId , @RequestBody Review review){
+    public ResponseEntity<String> addReview(@PathVariable Long companyId
+            , @RequestBody Review review){
+        boolean isReviewSaved = reviewService.addReview(companyId , review);
+        if (isReviewSaved)
+        return new ResponseEntity<>("Review Added Successfully" , HttpStatus.OK);
+        else
+           return new ResponseEntity<>("Review Not Saved" , HttpStatus.NOT_FOUND);
+    }
 
+    @GetMapping("/reviews/{reviewId}")
+    public ResponseEntity<Review> getReview(@PathVariable Long companyId ,
+                                            @PathVariable Long reviewId){
+        return new ResponseEntity<>(reviewService.getReview(companyId , reviewId) ,
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId ,
+                                               @PathVariable Long reviewId ,
+                                               @RequestBody Review review){
+        boolean isReviewUpdated = reviewService.updateReview(companyId , reviewId , review);
+        if (isReviewUpdated)
+            return new ResponseEntity<>("Review Updated Successfully" , HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review Not Updated" , HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable Long companyId ,
+                                               @PathVariable Long reviewId){
+        boolean isReviewDeleted = reviewService.deleteReview(companyId , reviewId);
+        if (isReviewDeleted)
+            return new ResponseEntity<>("Review deleted Successfully" , HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review Not deleted" , HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/org/firstjobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/org/firstjobapp/review/impl/ReviewServiceImpl.java
@@ -1,10 +1,13 @@
 package com.org.firstjobapp.review.impl;
 
+import com.org.firstjobapp.company.Company;
 import com.org.firstjobapp.company.CompanyService;
 import com.org.firstjobapp.review.Review;
 import com.org.firstjobapp.review.ReviewRepository;
 import com.org.firstjobapp.review.ReviewService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
@@ -26,7 +29,51 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public void addReview(Long companyId, Review review) {
-        reviewRepository.save()
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if (company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews.stream()
+                .filter(review -> review.getId().equals(reviewId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updatedReview) {
+        if (companyService.getCompanyById(companyId) != null){
+            updatedReview.setCompany(companyService.getCompanyById(companyId));
+            updatedReview.setId(reviewId);
+            reviewRepository.save(updatedReview);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteReview(Long companyId, Long reviewId) {
+        if (companyService.getCompanyById(companyId) != null
+        && reviewRepository.existsById(reviewId)){
+            Review review = reviewRepository.findById(reviewId).orElse(null);
+            Company company = review.getCompany();
+            company.getReviews().remove(review);
+            review.setCompany(null);
+            companyService.updateCompany(company , companyId);
+            reviewRepository.deleteById(reviewId);
+            return true;
+        }else {
+            return false;
+        }
     }
 }
